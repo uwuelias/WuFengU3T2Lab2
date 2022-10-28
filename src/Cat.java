8bfceb31db8fb5a59e9df2ceeaa345f518a0d4fb
BT_3W2_31 <hqiu4051@bths.edu>
2022-10-28 12:31:10 -0400

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,43 @@
+public class Cat
+{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    // add a getAge() getter below for
+    // obtaining the Cat's age
+
+
+
+    // MODIFY this method so that the Cat randomly
+    // returns one of three different messages
+    // (make any messages you want!) rather than always returning "meow".
+    // Each message should have equal chance of being returned.
+    public String speak() {
+        double num = (int) Math.random() * 4 + 1;
+        if (num == 1) {
+            return "meow!";
+        }
+        else if (num == 2) {
+            return "roar!";
+        }
+        else {
+            return "woof!";
+        }
+    }
+
+    public void introduce() {
+        System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int num = myScanner.nextInt();
+
+        // write your code here to print the appropriate message(s)
+        // described below
+        if (num < 1) {
+            System.out.println("That's not positive!");
+            System.exit(0);
+        }
+        if (num % 2 == 1) {
+            if (num % 5 == 0) {
+                System.out.println("Divisible by 5!");
+            }
+            if (num % 7 == 0) {
+                System.out.println("Divisible by 7!");
+            }
+            if ((num % 7 == 0 || num % 5 == 0) == false ) {
+                System.out.println("Not divisible by 5 or 7");
+            }
+        }
+        else {
+            if (num <= 100) {
+                System.out.println("Small even number");
+            }
+            if (num > 100) {
+                System.out.println("Medium even number");
+            }
+            if (num > 1000) {
+                System.out.println("Big even number");
+            }
+        }
+    }
+}
